0a5cb0f97820242df839bf6379b6872ece4eddd3
antonio.borojevic <antonio.borojevic@gmail.com>
2023-01-07 20:18:35 +0100

    change something on register

--- register.php
+++ register.php
@@ -60,16 +60,16 @@ if (isset($_POST['submit'])) {
 		<form action="" method="POST" class="login-email">
             <p class="login-text" style="font-size: 2rem; font-weight: 800;">Registration</p>
 			<div class="input-group">
-				<input type="text" placeholder="Ime" name="username" value="<?php echo $username; ?>" required>
+				<input type="text" placeholder="Name" name="username" value="<?php echo $username; ?>" required>
 			</div>
 			<div class="input-group">
 				<input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
 			</div>
 			<div class="input-group">
-				<input type="password" placeholder="Sifra" name="password" value="<?php echo $_POST['password']; ?>" required>
+				<input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
             </div>
             <div class="input-group">
-				<input type="password" placeholder="Potvrdite vasu sifru" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
+				<input type="password" placeholder="Again password" name="cpassword" value="<?php echo $_POST['cpassword']; ?>" required>
 			</div>
 			<div class="input-group">
 				<button name="submit" class="btn">Register</button>
